d625945bd2401087ee2ed839ed8aa31d5e6278b5
malindev07 <malindev07@yandex.ru>
2024-02-11 00:50:20 +0300

    new commit

---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type Users struct {
+	Users []User `json:"users"`
+}
+
+type User struct {
+	Name   string `json:"name"`
+	Number string `json:"number"`
+}
+
+// не знаю как выполнить задачу не объявляя структуру глобальной
+var users *Users
+
+// инициализация и проверка данных в json
+func checkJson() {
+	data, err := os.ReadFile("db.json")
+	err = json.Unmarshal(data, &users)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+// Поиск телефона по имени
+func (u *Users) FindNumber(name string) string {
+
+	for i := 0; i < len(users.Users); i++ {
+		if users.Users[i].Name == name {
+			return users.Users[i].Number
+		}
+	}
+	return "Указанное имя не найдено"
+}
+
+// Поиск имени по телефону
+func (u *Users) FindName(number string) string {
+	for i := 0; i < len(users.Users); i++ {
+		if users.Users[i].Number == number {
+			return users.Users[i].Name
+		}
+	}
+	return "Указанный телефон не найден"
+}
+
+// Удаление строки Имя : Телефон
+func (u *Users) DeleteRow(keyOrValue string) *Users {
+	for i := 0; i < len(users.Users); i++ {
+		if users.Users[i].Name == keyOrValue || users.Users[i].Number == keyOrValue {
+			users.Users = append(users.Users[:i], users.Users[i+1:]...)
+		}
+	}
+	data, _ := json.MarshalIndent(users, "", "\t")
+	_ = os.WriteFile("db.json", data, 02)
+	return users
+}
+
+// Вывод всего списка телефонной книги
+func (u *Users) ShowDB() *Users {
+	return users
+}
+
+// Добавление строки Имя : Телефон
+func (u *Users) AddRow(name, number string) *Users {
+	str := User{Name: name, Number: number}
+	users.Users = append(users.Users, str)
+	data, _ := json.MarshalIndent(users, "", "\t")
+	_ = os.WriteFile("db.json", data, 02)
+	return users
+
+}
+
+// обновление номера по имени
+func (u *Users) ChangeNumber(name, number string) *Users {
+	for i := 0; i < len(users.Users); i++ {
+		if users.Users[i].Name == name {
+			users.Users[i].Number = number
+		}
+	}
+	data, _ := json.MarshalIndent(users, "", "\t")
+	_ = os.WriteFile("db.json", data, 02)
+
+	return users
+
+}
+
+// далее идут обработчики на созданные функции
+func FindNumberHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, users.FindNumber("Lidia"))
+}
+func FindNameHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, users.FindName("89035043155"))
+}
+func ShowDBHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, users.ShowDB())
+}
+
+func DeleteRowHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, users.DeleteRow("Vladimir"))
+}
+
+func AddRowHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, users.AddRow("Vladimir", "89039460606"))
+}
+
+func ChangeHumberHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, users.ChangeNumber("Vladimir", "89039460605"))
+}
+
+func main() {
+	checkJson()
+	//wg := sync.WaitGroup{}
+	//wg.Add(2)
+	// вызываем обработчик с некой функцией внутри
+	go func() {
+		http.HandleFunc("/", ChangeHumberHandler)
+		//wg.Done()
+	}()
+	go func() {
+		//defer wg.Done()
+		//http.HandleFunc("/", FindNameHandler)
+
+	}()
+
+	//wg.Wait()
+	// Слушаем наш локальный сервер
+	http.ListenAndServe("localhost:8000", nil)
+
+}
